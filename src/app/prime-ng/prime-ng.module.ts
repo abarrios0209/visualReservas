9bd657ebb0c5cceddf65b1ed8490f6f815cf9378
abarrios0209 <80713264+abarrios0209@users.noreply.github.com>
2021-06-30 13:37:00 -0500

    spliter

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import {SplitterModule} from 'primeng/splitter';
+
+
+@NgModule({
+  exports: [
+    SplitterModule,
+  ]
+})
+export class PrimeNgModule { }
